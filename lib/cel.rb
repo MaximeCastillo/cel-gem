1b3741f45433e3b5e2c958c5a8b7a71e62346e99
Maxime Castillo <maxime@hop3team.com>
2024-08-20 12:25:02 +0200

    First commit

---
+++ lib/cel.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cel/version"
+
+module Cel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cel/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cel
+  VERSION = "0.1.0"
+end
